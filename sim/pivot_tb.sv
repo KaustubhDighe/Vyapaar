d9916b43d7dc52d86de7d6fd5c1d13cb3c617dc0
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2022-12-12 16:49:39 -0500

    Made pivot packed using genvar

--- sim/pivot_tb.sv
+++ sim/pivot_tb.sv
@@ -4,7 +4,7 @@
 module pivot_tb;
   logic clk;
   logic rst;
-  logic signed [15:0] matrix [2:0][2:0];
+  logic signed [2:0][2:0][15:0] matrix;
   logic [3:0] index[1:0];
 
   pivot #(.N_STOCKS(3)) dut(.matrix(matrix), .pivot_i(index[0]), .pivot_j(index[1]));
@@ -24,19 +24,20 @@ module pivot_tb;
     rst = 0;
     #20;
 
+    // Only symmetric matrices
     // Test 1
     $display("Test 1 ..");
-    matrix[0][0] = 16'b00000100000;
-    matrix[0][1] = 16'b00010000000;
-    matrix[0][2] = 16'b00000100000;
-    matrix[1][0] = 16'b00100010000;
-    matrix[1][1] = 16'b10000000000;
-    matrix[1][2] = 16'b00000000100;
-    matrix[2][0] = 16'b10000110000;
-    matrix[2][1] = 16'b10001000000;
-    matrix[2][2] = 16'b10000000000;
+    matrix[0][0] = 16'b000_0010_0000;
+    matrix[0][1] = 16'b010_1000_0000;
+    matrix[0][2] = 16'b000_0010_0000;
+    matrix[1][0] = 16'b010_1000_0000;
+    matrix[1][1] = 16'b111_0000_0000;
+    matrix[1][2] = 16'b100_0100_0100;
+    matrix[2][0] = 16'b000_0010_0000;
+    matrix[2][1] = 16'b100_0100_0000;
+    matrix[2][2] = 16'b100_0000_0000;
     #20;
-    if(index[0] == 2 && index[1] == 1) begin
+    if(index[0] == 1 && index[1] == 2) begin
         $display("Test 1 PASSED");
     end else begin
         $display("Test 1 FAILED : Check for off diagonal logic");
@@ -47,16 +48,16 @@ module pivot_tb;
     // Test 2
     $display("Test 2 ..");
     matrix[0][0] = 16'b0000_0100_0000_0000;
-    matrix[0][1] = 16'b0001_0000_0000_0000;
-    matrix[0][2] = 16'b1000_0100_0000_0000;
-    matrix[1][0] = 16'b0010_0010_0000_0000;
+    matrix[0][1] = 16'b0011_0010_0000_0000;
+    matrix[0][2] = 16'b1100_0100_0000_0000;
+    matrix[1][0] = 16'b0011_0010_0000_0000;
     matrix[1][1] = 16'b1000_0000_0000_0000;
-    matrix[1][2] = 16'b0000_0000_1000_0000;
-    matrix[2][0] = 16'b1000_0110_0000_0000;
+    matrix[1][2] = 16'b1000_1000_0000_0000;
+    matrix[2][0] = 16'b1100_0100_0000_0000;
     matrix[2][1] = 16'b1000_1000_0000_0000;
     matrix[2][2] = 16'b1000_0000_0000_0000;
     #20;
-    if(index[0] == 1 && index[1] == 0) begin
+    if(index[0] == 0 && index[1] == 1) begin
         $display("Test 2 PASSED");
     end else begin
         $display("Test 2 FAILED : Check for signed logic");
--- src/pivot.sv
+++ src/pivot.sv
@@ -4,28 +4,27 @@
 module pivot #(
     parameter WIDTH = 16,
     parameter N_STOCKS = 4) (
-  input signed [WIDTH - 1:0] matrix [N_STOCKS-1:0][N_STOCKS-1:0],
+  input signed [N_STOCKS-1:0][N_STOCKS-1:0][WIDTH - 1:0] matrix,
   output logic [$clog2(WIDTH) - 1:0] pivot_i,
   output logic [$clog2(WIDTH) - 1:0] pivot_j
   );
-  // This module is combinational
+  // This module is combinational and works only for symmetric matrices
   logic [$clog2(WIDTH) - 1:0] i_max, j_max;
   logic signed [WIDTH - 1: 0] max_val = -1;
-  always_comb begin
-    max_val = -1;
-    i_max = 0;
-    j_max = 0;
-    for (int i = 0; i < N_STOCKS; i++) begin
-        for (int j = 0; j < N_STOCKS; j++) begin
-            //$display(max_val);
-            if(i != j && matrix[i][j] > max_val) begin
-                max_val = matrix[i][j];
-                i_max = i;
-                j_max = j;
-            end
+  genvar i, j;
+  generate
+  for(i = 0; i < N_STOCKS; i++) begin
+    for(j = i+1; j < N_STOCKS; j++) begin
+      always @(*) begin
+        if(i != j && $signed(matrix[i][j]) > max_val) begin
+            max_val = $signed(matrix[i][j]);
+            i_max = i;
+            j_max = j;
         end
-    end 
+      end
+    end
   end
+  endgenerate
   assign pivot_i = i_max;
   assign pivot_j = j_max;
 endmodule
