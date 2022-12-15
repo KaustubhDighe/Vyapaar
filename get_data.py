ba4b697ca16ec7702e04b5c4ebf4c3c657c94711
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2022-12-14 20:24:29 -0500

    removed api key

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@ import http.client
 conn = http.client.HTTPSConnection("yahoo-finance15.p.rapidapi.com")
 
 headers = {
-    'X-RapidAPI-Key': "dc85f0fdacmsh38221138c62d3c4p1e47d6jsnfa3110f5c6b4",
+    'X-RapidAPI-Key': "",
     'X-RapidAPI-Host': "yahoo-finance15.p.rapidapi.com"
     }
 
