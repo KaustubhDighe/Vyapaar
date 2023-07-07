c4bd6e7eaa552951e4eacbf86d67922fdce399c7
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2023-07-07 16:29:56 -0400

    CORDIC arctan done

---
+++ sim/cordic_arctan_tb.sv
@@ -0,0 +1,153 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+module cordic_arctan_tb;
+  logic clk;
+  logic rst;
+  logic signed [15:0] y_in, x_in, theta_out;
+  logic valid_in, valid_out;
+
+  logic test_ok;
+
+  cordic_arctan dut(
+    .clk(clk),
+    .rst(rst),
+    .y_in(y_in),
+    .x_in(x_in),
+    .valid_in(valid_in),
+    .theta_out(theta_out),
+    .valid_out(valid_out)
+  );
+
+  always begin
+    #10;
+    clk = !clk;
+  end
+
+  initial begin
+    $dumpfile("arctan.vcd"); //file to store value change dump (vcd)
+    $dumpvars(0, cordic_arctan_tb); //store everything at the current level and below
+    $display("Starting Sim"); //print nice message
+    clk = 1;
+    rst = 1;
+    valid_in = 0;
+    #20;
+    rst = 0;
+    #20;
+
+    test_ok = 0;
+    // Test 1
+    $display("Test 1 (atan(1) = pi/4)..");
+    valid_in = 1;
+    y_in = 16'b0010_0000_0000_0000; // 1.0000
+    x_in = 16'b0010_0000_0000_0000; // 1.0000
+    #20;
+    valid_in = 0;
+    for(int i = 0; i < 10; i++) begin
+        if(valid_out && !test_ok) begin
+            if(theta_out == 16'h1921) begin
+                test_ok = 1;
+                $display(i);
+                $display("Test 1 PASSED");
+            end else begin
+                $display("Test 1 FAILED");
+                $display(theta_out);
+            end
+        end
+        #20;
+    end
+
+    test_ok = 0;
+    // Test 2
+    $display("Test 2 (atan(1/2) = 0.463647");
+    valid_in = 1;
+    y_in = 16'b0010_0000_0000_0000; // 1.0000
+    x_in = 16'b0100_0000_0000_0000; // 1.0000
+    #20;
+    valid_in = 0;
+    for(int i = 0; i < 13; i++) begin
+        if(valid_out && !test_ok) begin
+            if($signed(theta_out - 3798) <= 8 && $signed(theta_out - 3798) >= -8) begin
+                test_ok = 1;
+                $display(i);
+                $display("Test 2 PASSED");
+            end else begin
+                $display("Test 2 FAILED");
+                $display(theta_out);
+            end
+        end
+        #20;
+    end
+
+    test_ok = 0;
+    // Test 3
+    $display("Test 3 (atan(-1) = -pi/4");
+    valid_in = 1;
+    y_in = 16'b1110_0000_0000_0000; // -1.0000
+    x_in = 16'b0010_0000_0000_0000; // 1.0000
+    #20;
+    valid_in = 0;
+    for(int i = 0; i < 13; i++) begin
+        if(valid_out && !test_ok) begin
+            if($signed(theta_out - 16'hE6DD) <= 8 && $signed(theta_out - 16'hE6DD) >= -8) begin
+                test_ok = 1;
+                $display(i);
+                $display("Test 3 PASSED");
+            end else begin
+                $display("Test 3 FAILED");
+                $display("%h", theta_out - 16'hE6DD <= 8 );
+                $display(y_in);
+            end
+        end
+        #20;
+    end
+
+    test_ok = 0;
+    // Test 3
+    $display("Test 4 (atan(-1) = 3*pi/4");
+    valid_in = 1;
+    y_in = 16'b0010_0000_0000_0000; // 1.0000
+    x_in = 16'b1110_0000_0000_0000; // -1.0000
+    #20;
+    valid_in = 0;
+    for(int i = 0; i < 13; i++) begin
+        if(valid_out && !test_ok) begin
+            if($signed(theta_out - 16'h4B66) <= 8 && $signed(theta_out - 16'h4B66) >= -8) begin
+                test_ok = 1;
+                $display(i);
+                $display("Test 4 PASSED");
+            end else begin
+                $display("Test 4 FAILED");
+                $display("%h", theta_out - 16'h4b66 <= 8 );
+                $display(y_in);
+            end
+        end
+        #20;
+    end
+
+    test_ok = 0;
+    // Test 2
+    $display("Test 5 thrid quadrant");
+    valid_in = 1;
+    y_in = -$signed(16'b0100_0000_0000_0000); // -2.0000
+    x_in = -$signed(16'b0010_0000_0000_0000); // -1.0000
+    #20;
+    valid_in = 0;
+    for(int i = 0; i < 13; i++) begin
+        if(valid_out && !test_ok) begin
+            if($signed(theta_out + 16666) <= 8 && $signed(theta_out + 16666) >= -8) begin
+                test_ok = 1;
+                $display(theta_out);
+                $display("Test 5 PASSED");
+            end else begin
+                $display("Test 5 FAILED");
+                $display(theta_out);
+            end
+        end
+        #20;
+    end
+    $finish;
+  end
+endmodule
+
+`default_nettype wire
---
+++ src/cordic_arctan.sv
@@ -0,0 +1,92 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+// data_out = arctan(y_in/x_in)
+module cordic_arctan #(
+    parameter WIDTH = 16,
+    parameter STAGES = 10) (
+  input wire clk,
+  input wire rst,
+  input wire signed [WIDTH-1:0] y_in,
+  input wire signed [WIDTH-1:0] x_in,
+  input wire valid_in,
+  output logic signed [WIDTH-1:0] theta_out,
+  output logic valid_out
+  );
+  logic signed [WIDTH-1:0] theta = 0;
+  logic signed [WIDTH-1:0] x, y;
+  logic [1:0] state = 0;  // 0 - idle, 1 - processing, 2 - done
+  logic [$clog2(STAGES):0] i = 0;
+  logic done = 0;
+  
+  logic signed [9:0][WIDTH-1:0] atan_lut;
+  assign atan_lut[0] = 16'h1921;
+  assign atan_lut[1] = 16'h0ed6;
+  assign atan_lut[2] = 16'h07d6;
+  assign atan_lut[3] = 16'h03fa;
+  assign atan_lut[4] = 16'h01ff;
+  assign atan_lut[5] = 16'h00ff;
+  assign atan_lut[6] = 16'h007f;
+  assign atan_lut[7] = 16'h003f;
+  assign atan_lut[8] = 16'h001f;
+  assign atan_lut[9] = 16'h000f;
+
+  always_ff @( posedge clk ) begin
+    if(rst) begin
+        i <= 0;
+        theta <= 0;
+        state <= 0;
+        x <= 0;
+        y <= 0;
+        done <= 0;
+    end else if(state == 0) begin
+        if(valid_in) begin
+            
+            state <= 1;
+            if(x_in[WIDTH-1]) begin
+                x <= -x_in;
+                y <= -y_in;
+                if(y_in[WIDTH-1]) begin
+                    theta <= -$signed(16'h6488);
+                end else begin
+                    theta <= $signed(16'h6488);
+                end
+            end else begin
+                x <= x_in;
+                y <= y_in;
+                theta <= 0;
+            end
+        end else state <= 0;
+    end else if(state == 1) begin
+        if(y[WIDTH-1:0] > 0 && i < STAGES) begin
+            // $display("i: %d, y: %d", i, y);
+            if(y[WIDTH-1]) begin
+                x <= x - (y >>> i);
+                y <= y + (x >>> i);
+                theta  <= theta - $signed(atan_lut[i]);
+            end else begin
+                x <= x + (y >>> i);
+                y <= y - (x >>> i);
+                theta  <= theta + $signed(atan_lut[i]);
+            end
+            i <= i + 1;
+            if(i + 1 == STAGES) begin
+                state <= 2;
+                done <= 1;
+            end
+        end else begin
+            state <= 2;
+            done <= 1;
+        end
+    end else if(state == 2) begin
+        state <= 0;
+        done <= 0;
+        i <= 0;
+    end
+  end
+  
+  assign theta_out = theta;
+  assign valid_out = done;
+endmodule
+
+`default_nettype wire
